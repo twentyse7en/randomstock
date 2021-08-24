e2de11f5ed74827f15b79fd382918915f87351cb
Abijith <abhil@keyvalue.systems>
2021-08-24 23:28:36 +0530

    FIX: api bug

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,9 +1,9 @@
 const DEV = 'http://localhost:8000/api/'
-const PROD = 'https//randomstocknse.herokuapp.com/api/'
+const PROD = 'https//randomstocknse.herokuapp.com/'
 
 export const fetchRandomStock = async () => {
-	const endpoint = 'random/'
-	const resp = await fetch(PROD+endpoint, {
+	const endpoint = 'api/random/'
+	const resp = await fetch(endpoint, {
 		method: 'GET',
 	})
 	if(!resp.ok) {
